d6e26e83a2d9b334013eb4c9f64a8c26dfd90fc9
Borys35 <borykac10@gmail.com>
2020-04-07 15:09:50 +0200

    Improve 'play' command (playlists&search)

--- commands/voice/play.js
+++ commands/voice/play.js
@@ -1,12 +1,30 @@
 const { MessageEmbed } = require('discord.js');
 const ytdl = require('ytdl-core');
+const Youtube = require('simple-youtube-api');
+
+const youtube = new Youtube(process.env.YOUTUBE_API);
+
+async function addSong(video, message) {
+  const { queue } = message.guild.music;
+  const song = {
+    title: video.title,
+    url: video.url,
+    duration: video.durationSeconds,
+  };
+  const playing = !!queue.length;
+  queue.push(song);
+  if (!playing) {
+    const connection = await message.member.voice.channel.join();
+    play(connection, song, message);
+  }
+}
 
 function play(connection, song, message) {
-  const { title, video_url } = song;
+  const { title, url, duration } = song;
 
   const dispatcher = connection
     .play(
-      ytdl(video_url, {
+      ytdl(url, {
         filter: 'audioonly',
       })
     )
@@ -15,7 +33,7 @@ function play(connection, song, message) {
       const embed = new MessageEmbed()
         .setTitle('current song')
         .addField('title', title)
-        .setURL(video_url);
+        .setURL(url);
       message.channel.send(embed);
     })
     .on('finish', () => {
@@ -35,24 +53,43 @@ module.exports = {
   run: async (client, message, args) => {
     message.reply('yes your grace');
     if (message.member.voice.channel) {
-      const { queue } = message.guild.music;
-      const url = args[0];
-      if (!url)
-        return message.reply('you have to give song link after the command 😡');
-      try {
-        const playing = !!queue.length;
-        const song = (({ title, video_url }) => ({ title, video_url }))(
-          await ytdl.getInfo(url)
-        );
-        queue.push(song);
-
-        if (!playing) {
-          const connection = await message.member.voice.channel.join();
-          play(connection, song, message);
+      const query = args[0];
+      // IF NO ARG'S GIVEN
+      if (!query) {
+        return message.reply('you have to give arg after the command 😡');
+      }
+      // CHECK FOR PLAYLIST
+      else if (query.match(/^.*(youtu.be\/|list=)([^#\&\?]*).*/)) {
+        try {
+          const playlist = await youtube.getPlaylist(query);
+          const videos = await playlist.getVideos();
+          for (const video of videos) {
+            addSong(video, message);
+          }
+        } catch (err) {
+          console.error(err);
         }
-      } catch (err) {
-        console.error(err);
-        message.reply('invalid url 😡');
+      }
+      // NO PLAYLIST = VIDEO LINK
+      else if (
+        // http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)?‌​[\w\?‌​=]*)? TRY LATER
+        query.match(/^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+/)
+      ) {
+        try {
+          const video = await youtube.getVideo(query);
+          addSong(video, message);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+      // NO LINK = QUERY SEARCH
+      else {
+        try {
+          const videos = await youtube.searchVideos(query, 1);
+          addSong(videos[0], message);
+        } catch (err) {
+          console.error(err);
+        }
       }
     } else {
       message.reply('you have to be on a voice channel 😡');
